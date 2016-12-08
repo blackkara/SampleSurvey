dde249d67671b72f922fab2d9202184d7590fa0e
Mustafa <karamusti@gmail.com>
2016-12-08 14:04:53 +0300

    Centralize USay api files (url vs auth)
    ServiceApi needs to be init when app starting, to load url and auth values

--- app/src/main/java/com/blackkara/samplesurvey/SurveyApp.java
+++ app/src/main/java/com/blackkara/samplesurvey/SurveyApp.java
@@ -2,23 +2,13 @@ package com.blackkara.samplesurvey;
 
 import android.app.Application;
 
-import com.blackkara.samplesurvey.network.ApiAuth;
-import com.blackkara.samplesurvey.network.ApiUrl;
+import com.blackkara.samplesurvey.network.ServiceApi;
 
 public class SurveyApp extends Application{
 
     @Override
     public void onCreate() {
         super.onCreate();
-        initUSayAuth();
-        initUSayUrl();
-    }
-
-    private void initUSayAuth(){
-        ApiAuth.init(getApplicationContext());
-    }
-
-    private void initUSayUrl(){
-        ApiUrl.init(getApplicationContext());
+        ServiceApi.init(getApplicationContext());
     }
 }
--- app/src/main/java/com/blackkara/samplesurvey/network/ServiceApi.java
+++ app/src/main/java/com/blackkara/samplesurvey/network/ServiceApi.java
@@ -1,5 +1,9 @@
 package com.blackkara.samplesurvey.network;
 
+import android.content.Context;
+
+import com.blackkara.samplesurvey.R;
+
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
@@ -12,15 +16,26 @@ public class ServiceApi {
     private static final long TIMEOUT_CONN_IN_SECONDS = 15;
     private static final long TIMEOUT_READ_IN_SECONDS = 30;
 
-    private static ServiceApi sServiceApi;
-
     private IServiceApi mIServiceApi;
+
+    private static ServiceApi sServiceApi;
+    private static String sBaseUrl;
+    private static String sAuthToken;
 
     private ServiceApi(){}
 
-    public static ServiceApi getInstance(){
+    public static void init(Context context){
+        sBaseUrl = context.getString(R.string.base);
+        sAuthToken = context.getString(R.string.access_token);
+
         if (sServiceApi == null) {
             sServiceApi = new ServiceApi();
+        }
+    }
+
+    public static ServiceApi getInstance(){
+        if (sServiceApi == null) {
+            throw new IllegalStateException("Call ServiceApi.init(Context) before using");
         }
         return sServiceApi;
     }
@@ -30,10 +45,10 @@ public class ServiceApi {
             OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
             httpClientBuilder.readTimeout(TIMEOUT_READ_IN_SECONDS, TimeUnit.SECONDS);
             httpClientBuilder.connectTimeout(TIMEOUT_CONN_IN_SECONDS, TimeUnit.SECONDS);
-            httpClientBuilder.addInterceptor(new ApiAuthInterceptor(ApiAuth.getAuthToken()));
+            httpClientBuilder.addInterceptor(new ApiAuthInterceptor(sAuthToken));
 
             Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(ApiUrl.getBaseUrl())
+                    .baseUrl(sBaseUrl)
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io()))
                     .client(httpClientBuilder.build())
